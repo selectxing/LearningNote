30e4cba234782dd1ac1f7a784cc5b74fde025be9
selectxing <selectxing@gmail.com>
2018-11-19 17:45:19 +0800

    update 10 add tech

--- Python/10_进程和线程.py
+++ Python/10_进程和线程.py
@@ -1,5 +1,52 @@
 #进程和线程
 #多进程
+'''
+备注：UNIX/LINUX系统提供了fork()系统调用，Python的os包封装了fork，但win系统无法使用
+import os
 
+print('Process (%s) start...' % os.getpid())
+# Only works on Unix/Linux/Mac:
+pid = os.fork()
+if pid == 0:
+    print('I am child process (%s) and my parent is %s.' % (os.getpid(), os.getppid()))
+else:
+    print('I (%s) just created a child process (%s).' % (os.getpid(), pid))
+'''
+#win系统多进程
+from multiprocessing import Process
+import os
 
+def run_proc(name):
+    print('Run child process %s (%s)...' % (name, os.getpid()))
+
+if __name__=='__main__':
+    print('Parent process %s.' % os.getpid())
+    p = Process(target=run_proc, args=('test',))
+    print('Child process will start.')
+    p.start()
+    p.join() #等待子进程运行，不加这个父进程会先跑完
+    print('Child process end.')
+
+#进程池Pool
+from multiprocessing import Pool
+import os, time, random
+
+def long_time_task(name):
+    print('Run task %s (%s)...' % (name, os.getpid()))
+    start = time.time()
+    time.sleep(random.random() * 3)
+    end = time.time()
+    print('Task %s runs %0.2f seconds.' % (name, (end - start)))
+
+if __name__=='__main__':
+    print('Parent process %s.' % os.getpid())
+    p = Pool(4)
+    for i in range(5):
+        p.apply_async(long_time_task, args=(i,))
+    print('Waiting for all subprocesses done...')
+    p.close()
+    p.join()
+    print('All subprocesses done.')
+
+    
 #多线程
---
+++ Python/tech.py
@@ -0,0 +1,23 @@
+# import、from import、import as区别
+'''
+import：引入整个包，调用包中某个类时需要写包名
+from import：引入包中某个类，调用该类时无需再写包名
+import as：引用并设置别名
+'''
+
+import datetime
+print(datetime.datetime.now())
+
+from datetime import datetime
+print(datetime.now())
+
+import datetime as dt
+print(dt.datetime.now())
+
+from datetime import datetime as dt
+print(dt.now())
+
+# 如何简单地理解Python中的if __name__ == '__main__'
+'''
+https://blog.csdn.net/yjk13703623757/article/details/77918633/
+'''
